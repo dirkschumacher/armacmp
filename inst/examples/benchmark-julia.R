c1cc3712883d2d990923ce29dd9199c92498ccf8
Dirk Schumacher <mail@dirk-schumacher.net>
2019-08-08 19:55:47 +0200

    Add benchmark files
    Github close #13 and #23

---
+++ inst/examples/benchmark-julia.R
@@ -0,0 +1,147 @@
+library(armacmp)
+
+# some of julia's microbenchmarks translated to C++
+# https://github.com/JuliaLang/Microbenchmarks/blob/master/perf.R
+
+fib_cpp <- armacmp(function(n = type_scalar_int()) {
+  fib_rec <-  function(nr = type_scalar_int()) {
+    if (nr < 2) {
+      return(nr, type = type_scalar_int())
+    } else {
+      return(fib_rec(nr-1) + fib_rec(nr-2), type = type_scalar_int())
+    }
+  }
+  return(fib_rec(n), type = type_scalar_int())
+})
+stopifnot(fib_cpp(20) == 6765)
+fib_r <- function(n) {
+  fib_rec <-  function(nr) {
+    if (nr < 2) {
+      return(nr)
+    } else {
+      return(fib_rec(nr-1) + fib_rec(nr-2))
+    }
+  }
+  return(fib_rec(n))
+}
+stopifnot(fib_r(20) == 6765)
+
+microbenchmark::microbenchmark(
+  fib_cpp(20),
+  fib_r(20)
+)
+#> Unit: microseconds
+#>         expr      min       lq        mean    median         uq       max
+#>  fib_cpp(20)   67.005   68.797    80.96013   78.7445    81.9375   159.208
+#>    fib_r(20) 7784.130 8337.068 10444.88176 9735.8380 11110.0570 30726.564
+#>  neval
+#>    100
+#>    100
+
+pisum_cpp <- armacmp(function() {
+  t <- 0.0
+  for (j in seq_len(500L)) {
+    t <- 0.0
+    for (k in seq_len(10000L)) {
+      t <- t + 1.0/(k*k)
+    }
+  }
+  return(t, type = type_scalar_numeric())
+})
+stopifnot(abs(pisum_cpp()-1.644834071848065) < 1e-12)
+
+pisum_r <- function() {
+  t <- 0.0
+  for (j in seq_len(500L)) {
+    t <- 0.0
+    for (k in seq_len(10000L)) {
+      t <- t + 1.0/(k*k)
+    }
+  }
+  return(t)
+}
+stopifnot(abs(pisum_r()-1.644834071848065) < 1e-12)
+
+microbenchmark::microbenchmark(
+  pisum_cpp(),
+  pisum_r()
+)
+#> Unit: milliseconds
+#>         expr        min         lq       mean     median         uq
+#>  pisum_cpp()   7.405665   7.564064   8.359741   7.681684   8.138816
+#>    pisum_r() 234.937687 254.256766 335.728802 272.915697 347.099214
+#>        max neval
+#>   19.02528   100
+#>  938.94758   100
+
+# julia microbenchmark qsort
+qsort_r = function(a) {
+  qsort_kernel = function(lo, hi) {
+    i = lo
+    j = hi
+    while (i < hi) {
+      pivot = a[floor((lo+hi)/2)]
+      while (i <= j) {
+        while (a[i] < pivot) i = i + 1
+        while (a[j] > pivot) j = j - 1
+        if (i <= j) {
+          t = a[i]
+          a[i] <<- a[j]
+          a[j] <<- t
+          i = i + 1;
+          j = j - 1;
+        }
+      }
+      if (lo < j) qsort_kernel(lo, j)
+      lo = i
+      j = hi
+    }
+  }
+  qsort_kernel(1, length(a))
+  return(a)
+}
+
+qsort_cpp <- armacmp(function(vec = type_colvec()) {
+  vec_copy <- vec
+  qsort_kernel <- function(lo = type_scalar_int(), hi = type_scalar_int()) {
+    i <- lo
+    j <- hi
+    while (i < hi) {
+      pivot <- vec_copy[floor((lo+hi)/2)]
+      while (i <= j) {
+        while (vec_copy[i] < pivot) i <- i + 1
+        while (vec_copy[j] > pivot) j <- j - 1
+        if (i <= j) {
+          t <- vec_copy[i]
+          vec_copy[i] <- vec_copy[j]
+          vec_copy[j] <- t
+          i <- i + 1
+          j <- j - 1
+        }
+      }
+      if (lo < j) qsort_kernel(lo, j)
+      lo <- i
+      j <- hi
+    }
+  }
+  qsort_kernel(1, length(vec_copy))
+  return(vec_copy, type = type_colvec())
+})
+
+x <- runif(10000)
+all.equal(
+  as.numeric(qsort_cpp(x)),
+  qsort_r(x)
+)
+#> [1] TRUE
+microbenchmark::microbenchmark(
+  as.numeric(qsort_cpp(x)),
+  qsort_r(x)
+)
+#> Unit: microseconds
+#>                      expr       min        lq      mean     median
+#>  as.numeric(qsort_cpp(x))   780.364   813.185  1017.428   892.2985
+#>                qsort_r(x) 35739.019 38130.693 53444.287 44246.4470
+#>         uq        max neval
+#>   1107.884   2464.288   100
+#>  56029.359 392169.433   100
---
+++ inst/examples/benchmark-logreg.R
@@ -0,0 +1,83 @@
+library(armacmp)
+# Arnold, T., Kane, M., & Lewis, B. W. (2019). A Computational Approach to Statistical Learning. CRC Press.
+# logistic regression using the Newton-Raphson algorithm
+log_reg <- armacmp(function(X, y = type_colvec()) {
+  beta <- rep.int(0, ncol(X))
+  for (i in seq_len(25)) {
+    b_old <- beta
+    alpha <- X %*% beta
+    p <- 1 / (1 + exp(-alpha))
+    W <- p * (1 - p)
+    XtX <- crossprod(X, diag(W) %*% X)
+    score <- t(X) %*% (y - p)
+    delta <- solve(XtX, score)
+    beta <- beta + delta
+  }
+  return(beta, type = type_colvec())
+})
+
+log_reg_r_v1 <- function(X, y) {
+  beta <- rep.int(0, ncol(X))
+  for (i in seq_len(25)) {
+    b_old <- beta
+    alpha <- X %*% beta
+    p <- 1 / (1 + exp(-alpha))
+    W <- as.numeric(p * (1 - p))
+    XtX <- crossprod(X, diag(W) %*% X)
+    score <- t(X) %*% (y - p)
+    delta <- solve(XtX, score)
+    beta <- beta + delta
+  }
+  return(beta)
+}
+
+# a changed suggested by @JohnOrmerod that speeds up the R code
+# significantly
+log_reg_r_v2 <- function(X, y) {
+  beta <- rep.int(0, ncol(X))
+  for (i in seq_len(25)) {
+    b_old <- beta
+    alpha <- X %*% beta
+    p <- 1 / (1 + exp(-alpha))
+    W <- as.numeric(p * (1 - p))
+    XtX <- t(X * W) %*% X
+    score <- t(X) %*% (y - p)
+    delta <- solve(XtX, score)
+    beta <- beta + delta
+  }
+  beta
+}
+
+set.seed(50)
+n <- 1000 ; p <- 50
+true_beta <- rnorm(p)
+X <- cbind(1, matrix(rnorm(n * (p - 1)), ncol = p - 1))
+y <- runif(n) < plogis(X %*% true_beta)
+
+# to see that it actually does logistic regression
+all.equal(
+  as.numeric(log_reg(X, y)),
+  as.numeric(log_reg_r_v1(X, y))
+)
+#> [1] TRUE
+all.equal(
+  as.numeric(log_reg_r_v2(X, y)),
+  as.numeric(coef(glm.fit(X, y, family = binomial())))
+)
+#> Warning: glm.fit: fitted probabilities numerically 0 or 1 occurred
+#> [1] TRUE
+
+
+bench::mark(
+  log_reg(X, y),
+  log_reg_r_v1(X, y),
+  log_reg_r_v2(X, y)
+)
+#> Warning: Some expressions had a GC in every iteration; so filtering is
+#> disabled.
+#> # A tibble: 3 x 6
+#>   expression              min   median `itr/sec` mem_alloc `gc/sec`
+#>   <bch:expr>         <bch:tm> <bch:tm>     <dbl> <bch:byt>    <dbl>
+#> 1 log_reg(X, y)       54.49ms  61.18ms    15.7      10.8KB     0
+#> 2 log_reg_r_v1(X, y)    1.53s    1.53s     0.654   211.8MB     3.27
+#> 3 log_reg_r_v2(X, y)  77.45ms  82.86ms    11.9      30.6MB     9.93
